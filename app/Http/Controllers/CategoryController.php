b238f92cc296cb54ab828fe6a6247e5d12703723
robinatkins <90618494+robinatkins@users.noreply.github.com>
2023-04-01 00:42:27 -0300

    First Deliverable

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+use App\Item;
+use Session;
+
+class CategoryController extends Controller
+{
+
+    public function __construct()
+{
+    $this->middleware('auth');
+}
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::orderBy('name','ASC')->paginate(10);
+        $items = Item::all();
+        return view('categories.index')->with('categories',$categories)->with('items',$items);;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //validate the data
+        // if fails, defaults to create() passing errors
+        $this->validate($request, ['name'=>'required|max:100|unique:categories,name']); 
+
+        //send to DB (use ELOQUENT)
+        $category = new Category;
+        $category->name = $request->name;
+        $category->save(); //saves to DB
+
+        Session::flash('success','The category has been added');
+
+        //redirect
+        return redirect()->route('categories.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        return view('categories.edit')->with('category',$category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //validate the data
+        // if fails, defaults to create() passing errors
+        $category = Category::find($id);
+        $this->validate($request, ['name'=>"required|max:100|unique:categories,name,$id"]);             
+
+        //send to DB (use ELOQUENT)
+        $category->name = $request->name;
+
+        $category->save(); //saves to DB
+
+        Session::flash('success','The category has been updated');
+
+        //redirect
+        return redirect()->route('categories.index');
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+        $items = Item::all();
+        $itemListed = false;
+        // Ensure no items currently using category before deleting
+        // Scan 'items' table for 'category_id' of category to be deleted
+        foreach($items as $item)
+        {
+            if($item->category_id == $category->id)
+            {
+                $itemFound = true;
+                break;
+            }
+        }
+
+        // If no items found to be using category
+        if(!$itemListed)
+        {
+            // Execute delete f(x)
+            $category->delete();
+            Session::flash('success','The category has been deleted');
+        }
+        else
+        {
+            // Otherwise, category still contains item(s). Display error msg:
+            Session::flash('error','The category could not be deleted');
+
+        }
+        return redirect()->route('categories.index');
+    }
+}
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,117 @@
+<?php return array (
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'inertiajs/inertia-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Inertia\\ServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'jenssegers/agent' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Jenssegers\\Agent\\AgentServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Agent' => 'Jenssegers\\Agent\\Facades\\Agent',
+    ),
+  ),
+  'laravel/fortify' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Fortify\\FortifyServiceProvider',
+    ),
+  ),
+  'laravel/jetstream' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Jetstream\\JetstreamServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+);
